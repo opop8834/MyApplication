c8edd3dd2a49bef90bdd9a5119d4258927ab0614
opop8834 <opopop8834@naver.com>
2021-12-01 14:03:46 +0900

    commit connect app

--- app/src/main/java/com/example/myapplication/Developer_info.java
+++ app/src/main/java/com/example/myapplication/Developer_info.java
@@ -1,21 +1,21 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.Nullable;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.ImageView;
-import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Developer_info extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.developer_info);
+        TextView textView1 = (TextView) findViewById(R.id.text1) ;
+        textView1.setText("처음으로 만들어본 간단한 게임") ;
+
         Button back = (Button) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener(){
             @Override
--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -1,20 +1,22 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.Nullable;
-
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.View;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.ImageView;
-import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Game extends AppCompatActivity{
+
 
     ImageView imageDice;
     static int locate;
@@ -23,14 +25,48 @@ public class Game extends AppCompatActivity{
     static int randomai;
     static boolean win = true;
     static boolean lose = true;
+    static boolean anim = true;
+    int rand;
+    int randai;
+    int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
-        imageDice = findViewById(R.id.imageDice);
+        ImageView rail = (ImageView)findViewById(R.id.rail);
+        Glide.with(this).load(R.raw.rail_gif).into(rail);
+        ImageView rail2 = (ImageView)findViewById(R.id.rail2);
+        Glide.with(this).load(R.raw.rail_gif).into(rail2);
+        ImageView rail3 = (ImageView)findViewById(R.id.rail3);
+        Glide.with(this).load(R.raw.rail_gif).into(rail3);
+        ImageView rail4 = (ImageView)findViewById(R.id.rail4);
+        Glide.with(this).load(R.raw.rail_gif).into(rail4);
+        ImageView rail5 = (ImageView)findViewById(R.id.rail5);
+        Glide.with(this).load(R.raw.rail_gif).into(rail5);
+        ImageView rail6 = (ImageView)findViewById(R.id.rail6);
+        Glide.with(this).load(R.raw.rail_gif).into(rail6);
 
+        imageDice = findViewById(R.id.imageDice);
         Button bac = (Button) findViewById(R.id.bac);
+        int[] diceId = {R.drawable.dice1, R.drawable.dice2,
+                R.drawable.dice3, R.drawable.dice4,
+                R.drawable.dice5, R.drawable.dice6};
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (true)
+                {
+                    rand = getRandom(6, 0);
+                    imageDice.setImageResource(diceId[rand]);
+                }
+            }
+        };
+        timer.schedule(timerTask, 0, 500);
+
+
         bac.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -39,21 +75,32 @@ public class Game extends AppCompatActivity{
                 finish();
             }
         });
-
     }
+    public void onClick(View view) throws InterruptedException {
 
-    public void onClick(View view) {
-        int[] diceId = {R.drawable.dice1, R.drawable.dice2,
-                R.drawable.dice3, R.drawable.dice4,
-                R.drawable.dice5, R.drawable.dice6};
-        int rand = getRandom(6, 0);
-        int randai = getRandom(6, 0);
-        imageDice.setImageResource(diceId[rand]);
+        ImageView dice1 = (ImageView)findViewById(R.id.animDice);
+        final AnimationDrawable drawable = (AnimationDrawable) dice1.getDrawable();
+        randai = 3;
+
+        if (drawable.isRunning())
+        {
+            drawable.stop();
+            anim = true;
+        }
+        else
+        {
+            drawable.getCurrent();
+            drawable.start();
+
+            anim = false;
+        }
+
         final ImageView iv = (ImageView)findViewById(R.id.imagelion);
+        iv.bringToFront();
         final ImageView ivai = (ImageView)findViewById(R.id.image1);
+        ivai.bringToFront();
         if ( win == true && lose == true)
         {
-
 
             random = random + rand*50;
             randomai = randomai + randai*50;
@@ -79,7 +126,7 @@ public class Game extends AppCompatActivity{
                 lose = false;
             }
         }
-        else
+        else if (win == false || lose == false)
         {
             if (win == false && lose == true)
             {
